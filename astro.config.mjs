c31b1bf19895dd9c0c4ec4e9f1163beef898d277
santo <santo@solo.com>
2025-01-01 08:19:39 +0000

    astro.config.mjs including vite, lit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,17 @@ import react from '@astrojs/react';
 export default defineConfig({
   site: "https://the-golden-age.netlify.app",
   integrations: [react()],
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['lit'], // Externalize the 'lit' module
+      },
+    },
+  },
 });
+
+
+
+
+
+
